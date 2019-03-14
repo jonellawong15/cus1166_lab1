ceb0b568fc6f9afe07da63501e300a48fbaa4ee4
jonellawong15 <47012063+jonellawong15@users.noreply.github.com>
2019-03-14 14:10:58 -0400

    Update models.py

--- app.py/mymodules/models.py
+++ app.py/mymodules/models.py
@@ -1,1 +1,10 @@
-
+class student:
+   def set_grade(grade):
+      set_grade = grade
+      
+   def get_grade():
+      return grade
+    
+    def print_student_info():
+      print(student_name, grade, student_id)
+  
