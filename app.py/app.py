fc26d5ceffbe6163b2b3e8e8ed0f0aa77ad14187
jonellawong15 <47012063+jonellawong15@users.noreply.github.com>
2019-03-14 14:16:24 -0400

    Create app.py

---
+++ app.py/app.py
@@ -0,0 +1,3 @@
+l = ["Anna", "Tom", "John", "Dan", "Jonella", "Nicole", "Henry", "Joana", "Joyce", "John"]
+l = [70, 80, 60, 90, 85, 100, 90, 60, 80, 75]
+print(f" The average is {{average_grade(roster)}}")
